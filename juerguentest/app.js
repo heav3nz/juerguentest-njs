7dccca8acb2921ed610f0efb7e822afdd6574d71
Juerguen <jurg_92@hotmail.com>
2020-11-16 13:14:03 -0600

    agregando todo lo necesario en el proyecto backend para servir peticiones crud, using nodemon as devdependencie, morgan as http interceptor y bodyparser para request post futuras

---
+++ juerguentest/app.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    bodyParser = require ('body-parser'),
+    morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+
+
+
+app.use((req, resp, done)=> {
+    req.header('Access-control-Allow-Origin', '*');
+    req.header('Access-control-Allow-Headers', '*');
+    req.header('Access-control-Allow-Methods', '*');
+    req.header('allow', 'GET, POST, PUT, DELETE, OPTIONS');
+    done();
+});
+
+
+app.get('/test',(req, resp, done)=> {
+    resp.status(200).json({ info: 'todo genial?'});
+});
+
+app.use(function(req, resp, done) {
+    const err = new Error("Aparentemente, la ruta no fué encontrada");
+    err.status = 404;
+    done(err);
+});
+
+
+const server = app.listen(6968, ()=> {
+    console.log(`Escuchando el puerto 6968`);
+});
