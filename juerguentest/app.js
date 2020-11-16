99a9bf8cc05939f2d3b199ac315c1c2a273d6ba0
Juerguen <jurg_92@hotmail.com>
2020-11-16 13:19:36 -0600

    comentando el nodo de servidor principal

--- juerguentest/app.js
+++ juerguentest/app.js
@@ -5,24 +5,25 @@ const express = require('express'),
 const app = express();
 app.use(morgan('dev'));
 
-
-
-
+// permito cualquier acceso a mis endpoints por cuestiones de prueba.
 app.use((req, resp, done)=> {
     req.header('Access-control-Allow-Origin', '*');
     req.header('Access-control-Allow-Headers', '*');
     req.header('Access-control-Allow-Methods', '*');
     req.header('allow', 'GET, POST, PUT, DELETE, OPTIONS');
+    // permitiendo desde mis middlewares el uso de cualquier metodo-.
     done();
 });
 
+/* En caso de que se hagan peticiones sin destino en el servidor, retorna un 404
+con el sieguiente texto */
 app.use(function(req, resp, done) {
     const err = new Error("Aparentemente, la ruta no fué encontrada");
     err.status = 404;
     done(err);
 });
 
-
+// levanto servicios.
 const server = app.listen(6968, ()=> {
     console.log(`Escuchando el puerto 6968`);
 });
